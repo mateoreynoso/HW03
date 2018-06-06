872350a721ed2b907184cdfbbfe91110db507e6a
mateo <mateoreynosor@gmail.com>
2018-06-06 00:00:01 -0500

    second update

--- HW03-EX01/main.cpp
+++ HW03-EX01/main.cpp
@@ -1,4 +1,63 @@
+//////////////////////////
+// Mateo Reynoso
+// Data Structures
+// Homework 03
+/////////////////////////////
+
+// Written part
+// 1. For each of the following errors, what kind of exception should be thrown ?
+//	a.A subscript value that is equal to the number of elements in the array
+//	b.An index for a string that is less than 0
+//	c.A function return value that is too small to be represented
+//	d.A function return value that is too large to be represented
+//	e.An input operation that reads an invalid numeric string
+//	f.Attempting to open an input stream whose associated disk file cannot be
+//	found
+//	g.Attempting to allocate storage for a dynamic array when there is not sufficient
+//	memory available
+
+
+#include <iostream>
+#include <stdexcept>
+#include "readint.h"
+using namespace std;
+
 int main()
 {
-	return 0;
+	int low, high;
+	try
+	{
+		cin.exceptions(ios::failbit || ios::badbit);
+		cout << "Enter the range of values to read.\n";
+		cout << "high: ";
+		cin >> high;
+		cout << "low: ";
+		cin >> low;
+	}
+
+	catch (...)
+	{
+		std::cout << "Invalid input, try again. \n";
+		cin.clear();
+		cin.ignore(numeric_limits<int>::max(), '\n');
+		std::cout << "Input a number." << endl;
+	}
+
+	try
+	{
+		int number = read_int("Please enter a number within the range: ", low, high);
+		cout << "You entered " << number << endl;
+	}
+
+	catch (invalid_argument &e) 
+	{
+		cerr << "Exception: You supplied an invalid argument for read_int!\n";
+		cout << e.what() << endl;
+	}
+
+	
+
+	system("pause");
 }
+
+
---
+++ HW03-EX01/readInt.cpp
@@ -0,0 +1,49 @@
+#include "readInt.h"
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+int read_int(const string &prompt, int low, int high)
+
+{
+	// First checks the arguments
+	if (low > high)
+	{
+		throw invalid_argument("The smallest acceptable number cannot be higher that the largest acceptable integer.");
+	}
+
+	// Next asks the user for the input
+	int input;
+	
+	try
+	{
+		std::cout << prompt;
+		std:cin >> input;
+	}
+
+	// Makes sure the input is an int
+	catch (ios_base::failure &ex)
+	{
+		std::cout << "Invalid input, try again. \n";
+		cin.clear();
+		cin.ignore(numeric_limits<int>::max(), '\n');
+		std::cout << "Input a number." << endl;
+	}
+
+	// Now checks the input is between the parameters
+	bool correct = false;
+	do
+	{
+		if (input < low || high < input)
+		{
+			std::cout << "The input is out of range.\n";
+			std::cout << prompt;
+			std::cin >> input;
+		}
+		else
+			correct = true;
+	} while (correct);
+
+	return input;
+}
